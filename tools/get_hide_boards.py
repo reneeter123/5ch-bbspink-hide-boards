cc187c89ad967b0fa234bf35cef089ad3aee34be
ReNeeter <69535871+reneeter123@users.noreply.github.com>
2021-10-17 03:37:31 +0900

    BBSMENUを更新

--- tools/get_hide_boards.py
+++ tools/get_hide_boards.py
@@ -8,7 +8,7 @@ import urllib.parse
 import urllib.request
 
 servers = sys.argv[1].split(",")
-user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
 
 
 def urlopen_fakeua(url):
